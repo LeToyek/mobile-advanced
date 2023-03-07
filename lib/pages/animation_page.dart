03c342decdeda868306a8cb005c164fc532c4501
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2023-03-07 22:17:50 +0700

    [UPDATE] demo animate

--- lib/pages/animation_page.dart
+++ lib/pages/animation_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
 class AnimationPage extends StatelessWidget {
   static const route = "/animation";
@@ -8,9 +9,24 @@ class AnimationPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: const Text("data"),
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+            child: Container(
+                child: const Text(
+          "data",
+          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+        )
+                    .animate(
+                      onPlay: (controller) => controller.repeat(),
+                    )
+                    .color(
+                      begin: Colors.blue,
+                      end: Colors.purple,
+                    )
+                    .shake(duration: const Duration(seconds: 3))
+                    .shimmer()
+                    .slideY(begin: 2, end: -2, curve: Curves.ease))),
       ),
     );
   }
